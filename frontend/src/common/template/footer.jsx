1de2774afb6dc619481a24832fbea35c54b511bf
Wellington  Carvalho de Oliveira <wellingtondevops@gmail.com>
2020-07-29 11:44:16 -0300

    Feito até a aula 138

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props=>(
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2020
+            <a href='https://github.com/wellingtondevops/my-money-app' target='_blank'> WCOLIVER</a>
+        </strong>
+    </footer>
+)
